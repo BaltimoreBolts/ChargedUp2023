6b46ea559b1f1f87080ee68299d6199134263886
baltimorebolts6326 <baltimorebolts2019@gmail.com>
2023-02-01 20:50:03 -0500

    JJJ - init drive logic (copied from 2022)

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,9 +4,22 @@
 
 package frc.robot;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.RelativeEncoder;
+
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.PowerDistribution;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -15,6 +28,27 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * project.
  */
 public class Robot extends TimedRobot {
+
+  public CANSparkMax mLeftDriveMotor1;
+  public CANSparkMax mRightDriveMotor1;
+  public CANSparkMax mLeftDriveMotor2;
+  public CANSparkMax mRightDriveMotor2;
+  public DifferentialDrive mRobotDrive;
+  public MotorControllerGroup mLeftMotors;
+  public MotorControllerGroup mRightMotors;
+  public RelativeEncoder mLeftEncoder;
+  public RelativeEncoder mRightEncoder;
+
+  public Joystick mStick;
+  public XboxController mXbox;
+  public double mSpeed = 0.0;
+  public double mTwist = 0.0;
+
+  public double wheelDia = 6.0; // inches
+  public double gearRatio = 8.45; // Toughbox Mini
+  public double rWidth = 2; // robot width in inches
+
+
   private static final String kDefaultAuto = "Default";
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
@@ -29,6 +63,48 @@ public class Robot extends TimedRobot {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
+
+    // Drive Motors
+    mLeftDriveMotor1 = new CANSparkMax(3, MotorType.kBrushless);
+    mLeftDriveMotor2 = new CANSparkMax(4, MotorType.kBrushless);
+    mRightDriveMotor1 = new CANSparkMax(1, MotorType.kBrushless);
+    mRightDriveMotor2 = new CANSparkMax(2, MotorType.kBrushless);
+    mLeftMotors = new MotorControllerGroup(mLeftDriveMotor1, mLeftDriveMotor2);
+    mRightMotors = new MotorControllerGroup(mRightDriveMotor1, mRightDriveMotor2);
+
+    mLeftDriveMotor1.restoreFactoryDefaults();
+    mLeftDriveMotor2.restoreFactoryDefaults();
+    mRightDriveMotor1.restoreFactoryDefaults();
+    mRightDriveMotor2.restoreFactoryDefaults();
+    mLeftDriveMotor1.setSmartCurrentLimit(40);
+    mLeftDriveMotor2.setSmartCurrentLimit(40);
+    mRightDriveMotor1.setSmartCurrentLimit(40);
+    mRightDriveMotor2.setSmartCurrentLimit(40);
+    mLeftDriveMotor1.setIdleMode(CANSparkMax.IdleMode.kBrake);
+    mLeftDriveMotor2.setIdleMode(CANSparkMax.IdleMode.kBrake);
+    mRightDriveMotor1.setIdleMode(CANSparkMax.IdleMode.kBrake);
+    mRightDriveMotor2.setIdleMode(CANSparkMax.IdleMode.kBrake);
+
+    mLeftDriveMotor1.follow(mLeftDriveMotor2);
+    mRightDriveMotor1.follow(mRightDriveMotor2);
+
+    mLeftEncoder = mLeftDriveMotor1.getEncoder();
+    mRightEncoder = mRightDriveMotor1.getEncoder();
+
+    // Convert raw encoder units to inches using PIxdia/gearRatio
+    mLeftEncoder.setPositionConversionFactor((Math.PI * wheelDia) / gearRatio);
+    mRightEncoder.setPositionConversionFactor((Math.PI * wheelDia) / gearRatio);
+
+    mLeftDriveMotor1.setInverted(true);
+    mLeftDriveMotor2.setInverted(true);
+
+    mLeftDriveMotor1.burnFlash();
+    mLeftDriveMotor2.burnFlash();
+    mRightDriveMotor1.burnFlash();
+    mRightDriveMotor2.burnFlash();
+
+    mRobotDrive = new DifferentialDrive(mLeftMotors, mRightMotors);
+
   }
 
   /**
@@ -74,11 +150,23 @@ public class Robot extends TimedRobot {
 
   /** This function is called once when teleop is enabled. */
   @Override
-  public void teleopInit() {}
+  public void teleopInit() {
+    mRightEncoder.setPosition(0);
+    mLeftEncoder.setPosition(0);
+    mRobotDrive.arcadeDrive(0, 0);
+  }
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+
+    // Drive robot
+     mSpeed = -mStick.getY() * ((mStick.getThrottle() * -0.5) + 0.5);
+     mTwist = mStick.getTwist() * ((mStick.getThrottle() * -0.5) + 0.5);
+     mRobotDrive.arcadeDrive(mSpeed, mTwist);   
+
+    }
+
 
   /** This function is called once when the robot is disabled. */
   @Override
